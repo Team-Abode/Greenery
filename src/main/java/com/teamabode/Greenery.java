10e355b58fb5841501b6e069ae3f37c27b6cc2a6
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-07-23 19:56:30 -0500

    Setup

---
+++ src/main/java/com/teamabode/Greenery.java
@@ -0,0 +1,12 @@
+package com.teamabode;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Greenery implements ModInitializer {
+    public static final String MODID = "greenery";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/teamabode/GreeneryClient.java
@@ -0,0 +1,11 @@
+package com.teamabode;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GreeneryClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
